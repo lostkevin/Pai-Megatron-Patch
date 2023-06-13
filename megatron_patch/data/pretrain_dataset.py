22a89f756377b329025b239835f9bbd2821c4670
xiaocheng <xiaocheng.cjl>
2023-06-13 12:13:44 +0800

    set idxmap argument with new megatron

--- megatron_patch/data/pretrain_dataset.py
+++ megatron_patch/data/pretrain_dataset.py
@@ -335,7 +335,7 @@ class LLamaIdxMapDataset(torch.utils.data.Dataset):
         self.doc_idx, self.sample_idx, self.shuffle_idx, self.index_prefix = \
             _build_index_mappings(self.name, data_prefix,
                                   documents, self.indexed_dataset.sizes,
-                                  num_samples, self.max_padding_length-1, seed)
+                                  num_samples, self.max_padding_length-1, seed, data_cache_path=None)
 
     def __len__(self):
         # -1 is due to data structure used to retieve the index:
