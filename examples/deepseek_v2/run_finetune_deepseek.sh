8c353fe349da89ea4e74f242d4fbc3e671896cd5
同润 <jerry.lp@alibaba-inc.com>
2024-05-22 10:19:03 +0800

    Add Deepseek-V2 MoE Mcore implementation

--- examples/deepseek_v2/run_finetune_deepseek.sh
+++ examples/deepseek_v2/run_finetune_deepseek.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -e
-ENV=$1
-MEGATRON_PATCH_PATH=$2
-MEGATRON_PATH=${MEGATRON_PATCH_PATH}/Megatron-LM-240405
-export PYTHONPATH=${MEGATRON_PATH}:${MEGATRON_PATCH_PATH}:$PYTHONPATH
+
+CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+MEGATRON_PATH=$( dirname $( dirname ${CURRENT_DIR}))
+export PYTHONPATH=$PYTHONPATH:${MEGATRON_PATH}:${MEGATRON_PATH}/Megatron-LM-240405
 export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+ENV=$1
 if [ $ENV = dsw ]; then
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 MASTER_ADDR=localhost
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
 NNODES=1
 NODE_RANK=0
-GPUS_PER_NODE=8
+GPUS_PER_NODE=4
 
 elif [ $ENV = dlc ]; then
 
@@ -21,111 +23,106 @@ GPUS_PER_NODE=${KUBERNETES_CONTAINER_RESOURCE_GPU}
 
 fi
 
-DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
-
-MODEL_SIZE=$3
-BATCH_SIZE=$4
-GLOBAL_BATCH_SIZE=$5
-LR=$6
-MIN_LR=$7
-SEQ_LEN=$8
-PAD_LEN=$9
-EXTRA_VOCAB_SIZE=${10}
-PR=${11}
-TP=${12}
-PP=${13}
-AC=${14}
-DO=${15}
-FL=${16}
-SP=${17}
-TE=${18}
-MOE=${19}
-EP=${20}
-SAVE_INTERVAL=${21}
-DATASET_PATH=${22}
-VALID_DATASET_PATH=${23}
-PRETRAIN_CHECKPOINT_PATH=${24}
-TRAIN_ITERS=${25}
-LR_WARMUP_ITERS=${26}
-OUTPUT_BASEPATH=${27}
+MODEL_SIZE=$2
+BATCH_SIZE=$3
+GLOBAL_BATCH_SIZE=$4
+LR=$5
+MIN_LR=$6
+SEQ_LEN=$7
+PAD_LEN=$8
+PR=$9
+TP=${10}
+PP=${11}
+EP=${12}
+AC=${13}
+DO=${14}
+FL=${15}
+SP=${16}
+SAVE_INTERVAL=${17}
+DATASET_PATH=${18}
+VALID_DATASET_PATH=${19}
+PRETRAIN_CHECKPOINT_PATH=${20}
+TRAIN_ITERS=${21}
+LR_WARMUP_ITERS=${22}
+OUTPUT_BASEPATH=${23}
 
 
-if [ $MODEL_SIZE = 0.5B ]; then
+if [ $MODEL_SIZE = A21B ]; then
 
-NUM_LAYERS=24
-HIDDEN_SIZE=1024
-NUM_ATTN_HEADS=16
-INTERMEDIATE_SIZE=2816
-MAX_POSITION_EMBEDDINGS=32768
-gqa_options=""
-
-elif [ $MODEL_SIZE = 1.8B ]; then
-
-NUM_LAYERS=24
-HIDDEN_SIZE=2048
-NUM_ATTN_HEADS=16
-INTERMEDIATE_SIZE=5504
-MAX_POSITION_EMBEDDINGS=32768
-gqa_options=""
-
-elif [ $MODEL_SIZE = 4B ]; then
-
-NUM_LAYERS=40
-HIDDEN_SIZE=2560
-NUM_ATTN_HEADS=20
-INTERMEDIATE_SIZE=6912
-MAX_POSITION_EMBEDDINGS=32768
-gqa_options=""
-
-elif [ $MODEL_SIZE = 7B ]; then
-
-NUM_LAYERS=32
-HIDDEN_SIZE=4096
-NUM_ATTN_HEADS=32
-INTERMEDIATE_SIZE=11008
-MAX_POSITION_EMBEDDINGS=32768
-gqa_options=""
-
-elif [ $MODEL_SIZE = 14B ]; then
-
-NUM_LAYERS=40
 HIDDEN_SIZE=5120
-NUM_ATTN_HEADS=40
-INTERMEDIATE_SIZE=13696
-MAX_POSITION_EMBEDDINGS=32768
-gqa_options=""
+NUM_ATTN_HEADS=128
+NUM_LAYERS=1
+INTERMEDIATE_SIZE=12288
+MOE_INTERMEDIATE_SIZE=1536
+SHARED_EXPERT_INTERMEDIATE_SIZE=1536
+MAX_POSITION_EMBEDDINGS=163840
+EXTRA_VOCAB_SIZE=2400
+Q_LORA_RANK=1536
+KV_LORA_RANK=512
+QK_NOPE_HEAD_DIM=128
+QK_ROPE_HEAD_DIM=64
+V_HEAD_DIM=128
+ROPE_THETA=10000
+SCALE_FACTOR=40
+NUM_EXPERTS=160
+ROUTER_TOPK=1
+NUM_SHARED_EXPERTS=2
+MOE_LAYER_FREQ=1
 
-elif [ $MODEL_SIZE = 32B ]; then
+moe_options=" \
+    --moe-ffn-hidden-size ${MOE_INTERMEDIATE_SIZE} \
+    --shared-moe-ffn-hidden-size ${SHARED_EXPERT_INTERMEDIATE_SIZE} \
+    --enable-shared-expert \
+    --moe-layer-freq ${MOE_LAYER_FREQ} \
+    --num-shared-experts ${NUM_SHARED_EXPERTS} \
+    --moe-router-topk ${ROUTER_TOPK} \
+    --num-experts ${NUM_EXPERTS} \
+    --moe-aux-loss-coeff 1e-2 \
+    --expert-model-parallel-size ${EP} \
+    --q-lora-rank ${Q_LORA_RANK} \
+    --kv-lora-rank ${KV_LORA_RANK} \
+    --qk-nope-head-dim ${QK_NOPE_HEAD_DIM} \
+    --qk-rope-head-dim ${QK_ROPE_HEAD_DIM} \
+    --v-head-dim ${V_HEAD_DIM} \
+    --moe-router-load-balancing-type aux_loss"
 
-NUM_LAYERS=64
-HIDDEN_SIZE=5120
-NUM_ATTN_HEADS=40
-INTERMEDIATE_SIZE=27392
-MAX_POSITION_EMBEDDINGS=32768
 
-gqa_options=" \
-		    --group-query-attention \
-		    --num-query-groups 8"
-
-elif [ $MODEL_SIZE = 72B ]; then
-
-NUM_LAYERS=80
-HIDDEN_SIZE=8192
-NUM_ATTN_HEADS=64
-INTERMEDIATE_SIZE=24576
-MAX_POSITION_EMBEDDINGS=32768
-gqa_options=""
-
-elif [ $MODEL_SIZE = A2.7B ]; then
+elif [ $MODEL_SIZE = A2.4B ]; then
 
 HIDDEN_SIZE=2048
 NUM_ATTN_HEADS=16
-NUM_LAYERS=24
-INTERMEDIATE_SIZE=5632
+NUM_LAYERS=27
+INTERMEDIATE_SIZE=10944
 MOE_INTERMEDIATE_SIZE=1408
-SHARED_EXPERT_INTERMEDIATE_SIZE=5632
-MAX_POSITION_EMBEDDINGS=8192
-gqa_options=""
+SHARED_EXPERT_INTERMEDIATE_SIZE=1408
+MAX_POSITION_EMBEDDINGS=163840
+EXTRA_VOCAB_SIZE=2400
+KV_LORA_RANK=512
+QK_NOPE_HEAD_DIM=128
+QK_ROPE_HEAD_DIM=64
+V_HEAD_DIM=128
+ROPE_THETA=10000
+SCALE_FACTOR=40
+NUM_EXPERTS=64
+ROUTER_TOPK=1
+NUM_SHARED_EXPERTS=2
+MOE_LAYER_FREQ=1
+
+moe_options=" \
+    --moe-ffn-hidden-size ${MOE_INTERMEDIATE_SIZE} \
+    --shared-moe-ffn-hidden-size ${SHARED_EXPERT_INTERMEDIATE_SIZE} \
+    --enable-shared-expert \
+    --moe-layer-freq ${MOE_LAYER_FREQ} \
+    --num-shared-experts ${NUM_SHARED_EXPERTS} \
+    --moe-router-topk ${ROUTER_TOPK} \
+    --num-experts ${NUM_EXPERTS} \
+    --moe-aux-loss-coeff 1e-2 \
+    --expert-model-parallel-size ${EP} \
+    --kv-lora-rank ${KV_LORA_RANK} \
+    --qk-nope-head-dim ${QK_NOPE_HEAD_DIM} \
+    --qk-rope-head-dim ${QK_ROPE_HEAD_DIM} \
+    --v-head-dim ${V_HEAD_DIM} \
+    --moe-router-load-balancing-type aux_loss"
 
 fi
 
@@ -174,39 +171,6 @@ elif [ $FL = false ]; then
                     "
 fi
 
-if [ $TE = true ]; then
-    te_options=" \
-		    --transformer-impl transformer_engine"
-
-elif [ $TE = false ]; then
-    te_options=" \
-        --transformer-impl local"
-fi
-
-if [ $MOE = true ]; then
-    if [ $MODEL_SIZE = A2.7B ]; then
-        moe_options=" \
-            --moe-ffn-hidden-size ${MOE_INTERMEDIATE_SIZE} \
-            --shared-moe-ffn-hidden-size ${SHARED_EXPERT_INTERMEDIATE_SIZE} \
-            --enable-shared-expert \
-            --moe-router-topk 4 \
-            --num-experts 60 \
-            --moe-aux-loss-coeff 1e-2 \
-            --expert-model-parallel-size ${EP} \
-            --moe-router-load-balancing-type aux_loss"
-    else
-        moe_options=" \
-            --moe-router-topk 2 \
-            --num-experts 8 \
-            --moe-aux-loss-coeff 1e-2 \
-            --expert-model-parallel-size ${EP} \
-            --moe-router-load-balancing-type aux_loss"
-    fi
-
-elif [ $MOE = false ]; then
-    moe_options=" \
-                    "
-fi
 
 if [ $SP = true ] && [ $TP -gt 1 ]; then
     sp_options=" \
@@ -224,7 +188,7 @@ fi
 
 LR_DECAY_ITERS=$(( ${TRAIN_ITERS} - ${LR_WARMUP_ITERS}))
 
-NAME="${ENV}-finetune-megatron-qwen2-${MODEL_SIZE}-lr-${LR}-bs-${BATCH_SIZE}-seqlen-${SEQ_LEN}-pr-${PR}-tp-${TP}-pp-${PP}-ac-${AC}-do-${DO}-sp-${SP}-tt-${TRAIN_TOKENS}-wt-${WARMUP_ITERS}"
+NAME="${ENV}-finetune-megatron-deepseek-${MODEL_SIZE}-lr-${LR}-bs-${BATCH_SIZE}-seqlen-${SEQ_LEN}-pr-${PR}-tp-${TP}-pp-${PP}-ac-${AC}-do-${DO}-sp-${SP}-tt-${TRAIN_TOKENS}-wt-${WARMUP_ITERS}"
 mkdir -p "${OUTPUT_BASEPATH}/tensorboard/"
 mkdir -p "${OUTPUT_BASEPATH}/checkpoint/"
 mkdir -p "${OUTPUT_BASEPATH}/log/"
@@ -287,16 +251,18 @@ megatron_options="  \
         --position-embedding-type rope \
         --untie-embeddings-and-output-weights \
         --disable-bias-linear \
-        --add-qkv-bias \
         --use-mcore-models \
-        --rotary-percent 1.0 \
-        --rotary-base 1000000 \
-        --rotary-seq-len-interpolation-factor 1 \
+        --rotary-base ${ROPE_THETA} \
+        --rotary-seq-len-interpolation-factor ${SCALE_FACTOR} \
+        --transformer-impl transformer_engine \
         --eod-mask-loss
         "
 
-run_cmd="torchrun $DISTRIBUTED_ARGS pretrain_mcore_qwen.py
- ${megatron_options} ${pr_options} ${load_options} ${te_options} ${activation_checkpoint_options} ${do_options} ${flash_options} ${sp_options} ${gqa_options} ${moe_options}"
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+
+run_cmd="torchrun $DISTRIBUTED_ARGS pretrain_deepseek.py
+ ${megatron_options} ${pr_options} ${load_options} ${activation_checkpoint_options} ${do_options} ${flash_options} ${sp_options} ${moe_options}"
 
 echo ${run_cmd}
 eval ${run_cmd}
