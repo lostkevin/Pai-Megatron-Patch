76138a45f28dd6364a4536593cc4fc6831e4c35a
jerry.lp <jerry.lp@alibaba-inc.com>
2025-02-17 14:13:11 +0800

    [to #63453759]Using Megatron LM 250213 backend to train deepseek-v3 Link: https://code.alibaba-inc.com/PAI-TL/PAI-Megatron-Patch/codereview/20448548 * Using Megatron LM 250213 backend to train deepseek-v3
    * Using Megatron LM 250213 backend to train deepseek-v3
    
    * Using Megatron LM 250213 backend to train deepseek-v3
    
    * Using Megatron LM 250213 backend to train deepseek-v3

--- examples/deepseek_v3/pretrain_deepseek.py
+++ examples/deepseek_v3/pretrain_deepseek.py
@@ -15,9 +15,10 @@
 import os
 from functools import partial
 from typing import Union
-
+from contextlib import nullcontext
 import torch
 import torch._dynamo
+import megatron.legacy.model
 from megatron.core import mpu
 from megatron.core.datasets.blended_megatron_dataset_builder import (
     BlendedMegatronDatasetBuilder,
@@ -37,57 +38,91 @@ from megatron.training.utils import (
     get_batch_on_this_tp_rank,
 )
 
+from megatron.training.arguments import core_transformer_config_from_args
+from megatron.core.models.gpt import GPTModel
 
+
+from megatron_patch.model.deepseek_v3.layer_specs import (
+    get_gpt_decoder_block_spec,
+    get_gpt_layer_local_spec,
+    get_gpt_layer_with_transformer_engine_spec,
+)
 from megatron_patch.arguments import get_patch_args
 from megatron_patch.tokenizer import build_tokenizer, get_tokenizer
 from megatron_patch.data import build_pretrain_dataset_from_original
 from megatron_patch.data.utils import get_batch_on_this_tp_rank_original, get_batch_on_this_tp_rank_idxmap_sft
 
-from megatron_patch.model.deepseek_v2.transformer_config  import core_transformer_config_from_args
-from megatron_patch.model.deepseek_v2.transformer_config import DeepSeekV2TransformerConfig
-from megatron_patch.model.deepseek_v2.model import GPTModel
-from megatron_patch.model.deepseek_v2.layer_specs import (
-    get_gpt_layer_with_transformer_engine_spec,
-)
 
 torch._dynamo.config.suppress_errors = True
 
+def model_provider(pre_process=True, post_process=True) -> Union[GPTModel, megatron.legacy.model.GPTModel]:
+    """Builds the model.
 
-def model_provider(
-    pre_process=True, post_process=True
-) -> Union[GPTModel]:
+    If you set the use_legacy_models to True, it will return the legacy GPT model and if not the mcore GPT model.
 
+    Args:
+        pre_process (bool, optional): Set to true if you need to compute embedings. Defaults to True.
+        post_process (bool, optional): Set to true if you need to want to compute output logits/loss. Defaults to True.
+
+
+    Returns:
+        Union[GPTModel, megatron.legacy.model.GPTModel]: The returned model
+    """
     args = get_args()
     build_tokenizer(args)
-    config = core_transformer_config_from_args(args, DeepSeekV2TransformerConfig)
     use_te = args.transformer_impl == "transformer_engine"
 
-    if use_te:
-        print_rank_0("building deepseek_v2 model in TE...")
-        transformer_layer_spec = get_gpt_layer_with_transformer_engine_spec(
-            args.num_experts, args.moe_grouped_gemm, args.qk_layernorm, multi_latent_attention=True, fp8=args.fp8
-        )
-    else:
-        raise ValueError("Current only support TE")
+    print_rank_0('building GPT model ...')
 
-    model = GPTModel(
-        config=config,
-        transformer_layer_spec=transformer_layer_spec,
-        vocab_size=args.padded_vocab_size,
-        max_sequence_length=args.max_position_embeddings,
-        pre_process=pre_process,
-        post_process=post_process,
-        fp16_lm_cross_entropy=args.fp16_lm_cross_entropy,
-        parallel_output=True,
-        share_embeddings_and_output_weights=not args.untie_embeddings_and_output_weights,
-        position_embedding_type=args.position_embedding_type,
-        rotary_percent=args.rotary_percent,
-        rotary_base=args.rotary_base,
-        seq_len_interpolation_factor=args.rotary_seq_len_interpolation_factor,
-    )
+    config = core_transformer_config_from_args(args)
+
+    if args.num_experts:
+        # Define the decoder block spec
+        transformer_layer_spec = get_gpt_decoder_block_spec(config, use_transformer_engine=use_te)
+    else:
+        # Define the decoder layer spec
+        if use_te:
+            transformer_layer_spec = get_gpt_layer_with_transformer_engine_spec(
+                args.num_experts, args.moe_grouped_gemm,
+                args.qk_layernorm, args.multi_latent_attention, args.moe_use_legacy_grouped_gemm)
+        else:
+            transformer_layer_spec = get_gpt_layer_local_spec(
+                args.num_experts, args.moe_grouped_gemm,
+                args.qk_layernorm, args.multi_latent_attention, args.moe_use_legacy_grouped_gemm)
+
+    build_model_context = nullcontext
+    build_model_context_args = {}
+    if args.fp8_param_gather:
+        try:
+            from transformer_engine.pytorch import fp8_model_init
+
+            build_model_context = fp8_model_init
+            build_model_context_args["enabled"] = True
+
+            # Check if fp8_model_init supports preserve_high_precision_init_val
+            if "preserve_high_precision_init_val" in inspect.signature(fp8_model_init).parameters:
+                build_model_context_args["preserve_high_precision_init_val"] = True
+        except:
+            raise RuntimeError("--fp8-param-gather requires `fp8_model_init` from TransformerEngine, but not found.")
+
+    with build_model_context(**build_model_context_args):
+        model = GPTModel(
+            config=config,
+            transformer_layer_spec=transformer_layer_spec,
+            vocab_size=args.padded_vocab_size,
+            max_sequence_length=args.max_position_embeddings,
+            pre_process=pre_process,
+            post_process=post_process,
+            fp16_lm_cross_entropy=args.fp16_lm_cross_entropy,
+            parallel_output=True,
+            share_embeddings_and_output_weights=not args.untie_embeddings_and_output_weights,
+            position_embedding_type=args.position_embedding_type,
+            rotary_percent=args.rotary_percent,
+            rotary_base=args.rotary_base,
+            rope_scaling=args.use_rope_scaling
+        )
 
     return model
-
 
 def get_batch(data_iterator):
     """Generate a batch."""
